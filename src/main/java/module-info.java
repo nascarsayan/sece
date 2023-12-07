7d2757b60dc95c7f0a6ce62549f9110a8b3c0803
Sayan Naskar <nascarsayan@gmail.com>
2023-12-07 14:04:41 +0530

    remove sample

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.librarymanagement {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.librarymanagement to javafx.fxml;
-    exports com.example.librarymanagement;
-}
